b4bf2b18056c568a13ddaa2e286aeb9c9debed75
H.Kubota <mini.kensington.29@gmail.com>
2021-03-07 11:21:06 +0900

    Udacity  Developing Android Apps with Kotlin   Lesson 1   18.Exercise:Change the Textのbuildでエラー発生。   MainActivity.ktの27行、randomIntがunknown。   import java.util.*を追加。   なお、kotlin,random.RandomのインポートではNG。

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+// import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
